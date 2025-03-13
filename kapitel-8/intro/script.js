e06c9dd17b9f936e7fa5c94d8c4994f1ec4ab225
georgios.kalanidis <georgios.kalanidis@elev.ga.ntig.se>
2025-03-13 14:27:08 +0100

    Tränat på javascript, variabler, konkatenering.

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,16 @@
+//var tal1 =12;
+//var tal2 =34;
+//var summa = tal1 +tal2;
+//alert(tal1 + " + " + tal2 +" = " + summa);
+
+var förnamn = "Georgios";
+var efternamn = " Kalanidis";
+var ålder = 17;
+var namn = förnamn + " " + efternamn;
+//var meddelande = namn + "är" + ålder+ "år gammal."
+//alert(meddelande);
+
+var hobby = "MMA";
+var kostnad = 500;
+var meddelande = namn + " gillar " + hobby + "\noch det kostar " + kostnad + per år;
+alert(meddelande);
